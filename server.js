7ea4c568ce33c61aa9ecbfed539a737b1967c9e6
Sky K <skyj.kim7@gmail.com>
2023-02-26 19:35:13 -0800

    added middle ware and PORT access

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const path = require ('path');
 const api = require ('./routes/index.js');
+
+
+const PORT = process.envPORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
