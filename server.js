83751fb27a33c5409edc92ec951f148b89511286
Sky K <skyj.kim7@gmail.com>
2023-02-26 19:37:05 -0800

    added GET routes to notes and all

--- server.js
+++ server.js
@@ -9,3 +9,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('./notes', (req,res)=>
+    res.sendFile(path.join(__dirname, 'public/notes.html')))
+
+app.get('*', (req,res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+    );
