7af472672fa80c6dca63872601cf175cb9cd01d2
Sky K <skyj.kim7@gmail.com>
2023-02-26 19:44:42 -0800

    added async/await

--- routes/notes.js
+++ routes/notes.js
@@ -4,3 +4,10 @@ const fs = require ("fs");
 const uuid = require ("../helpers/uuid");
 
 
+notes.get('/', async(req,res)=>{
+    let data = fs.readFileSync(`db/db.json`, "utf8")
+    data= JSON.parse(data)
+    res.json(data)
+
+    console.log(`${req.method} request recieved for reviews`);
+});
